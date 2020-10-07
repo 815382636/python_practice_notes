288b386a187e5a17cf791de5ad893d514ea82396
815382636 <815382636@qq.com>
2020-10-07 14:37:19 +0800

    modify

--- new_skill_with.py
+++ new_skill_with.py
@@ -13,22 +13,26 @@ class DBConnectionManager:
 
 
 with DBConnectionManager('localhost', '8080') as db_client:
-    class FileManager:
-        def __init__(self, name, mode):
-            print('calling __init__ method')
-            self.name = name
-            self.mode = mode
-            self.file = None
+    pass
 
-        def __enter__(self):
-            print('calling __enter__ method')
-            self.file = open(self.name, self.mode)
-            return self.file
 
-        def __exit__(self, exc_type, exc_val, exc_tb):
-            print('calling __exit__ method')
-            if self.file:
-                self.file.close()
+class FileManager:
+    def __init__(self, name, mode):
+        print('calling __init__ method')
+        self.name = name
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        print('calling __enter__ method')
+        self.file = open(self.name, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('calling __exit__ method')
+        if self.file:
+            self.file.close()
+
 
 with FileManager('test.txt', 'w') as f:
     print('ready to write to file')
